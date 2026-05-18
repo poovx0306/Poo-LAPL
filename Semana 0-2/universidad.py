45ede95f29116c6786bce28e81065b2e5638f922
poovx0306 <angel.poovx@gmail.com>
2026-05-18 13:34:36 +0000

    Codigo de la universidad

---
+++ Semana 0-2/universidad.py
@@ -0,0 +1,27 @@
+class Universidad: 
+    def __init__(self, logo, oferta_educativa, localidad, sistema_informacion, modalidad, servicios, ubicacion, talleres, cantidad_salones, rector):
+        self.logo = logo
+        self.oferta_educativa = oferta_educativa
+        self. localidad = localidad
+        self. sistema_informacion = sistema_informacion
+        self. modalidad = modalidad
+        self. servicios = servicios
+        self. ubicacion = ubicacion
+        self. talleres = talleres
+        self. cantidad_salones = cantidad_salones
+        self. rector = rector
+        print (f"Logo: {self.logo}")
+        print (f"Oferta Educativa {self.oferta_educativa}")
+        print (f"Localidad {self.localidad}")
+        print (f"Sistema de Información: {sistema_informacion}")
+        print (f"Modalidad {modalidad}")
+        print (f"Servicios {servicios}")
+        print (f"Ubicacion {ubicacion}")
+        print (f"Talleres {talleres}")
+        print (f"Cantidad de salones: {cantidad_salones}")
+        print (f"Rector: {rector}")
+        
+        
+
+
+                
--- universidad.py
+++
@@ -1,8 +0,0 @@
-class Universidad: 
-    def __init__(self, logo, oferta_educativa, localidad, sistema_informacion, modalidad, servicios, ubicacion, talleres, cantidad_salones, rector):
-        self.logo = logo
-        self.oferta_educativa = oferta_educativa
-        self. localidad = localidad
-        self. sistema_informacion = sistema_informacion
-        self. modalidad = modalidad
-        
