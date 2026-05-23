697c92202801816ed54e9d7dd79968f1dec847bf
poovx0306 <angel.poovx@gmail.com>
2026-05-23 03:19:18 +0000

    Actualizaciones

--- ejercicios/semana_02/silla.py
+++ ejercicios/semana_02/silla.py
@@ -1,4 +1,13 @@
 class Silla:
     def __init__(self, color, anchura, altura, forma, material, peso_max,estilo,patas,marca,respaldo):
         self.color = color
-        self.anchura = anchura        
+        self.anchura = anchura
+        self.altura = altura
+        self.forma = forma
+        self.material = material
+        self.peso_max = peso_max
+        self.estilo = estilo
+        self.patas = patas
+        self.marca = marca
+        self.respaldo = respaldo
+            
